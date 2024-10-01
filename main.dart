4ec659498c2021b202cb9d1e693fc677da45f067
Kuwait-zero <hussein-_2000@hotmail.com>
2024-10-01 02:53:17 -0400

    Task: Dart Conditionals

---
+++ main.dart
@@ -0,0 +1,53 @@
+void main(){
+int mark = 70;
+
+  if (mark >= 80 )
+    print("A");
+      else if ( mark >=70 && mark <80 )
+    print("b");
+       else if ( mark >=60 && mark <70 )
+    print("c");
+     else  if ( mark >=50 && mark <60 )
+    print("d");
+     else  if ( mark <50 )
+    print("f");
+
+
+   int  i = 5;
+   
+    if (i % 3 == 0 && i % 5 == 0) 
+      print("fizzBuzz");
+     else if (i % 3 == 0) 
+      print("fizz");
+     else if (i % 5 == 0) 
+      print("buzz");
+      else 
+      print(i);
+    
+
+// Using a switch statements, create a calculator with ("+", "-", "/", "*") and variables (x, y, operation).
+var sign = '/';
+int x= 1;
+int y = 4;
+
+switch(sign) {
+
+  case '+':
+print (x+y);
+  case'*':
+print (x+y);
+  case '/':
+print (x/y);
+  case '-':
+print (x-y);
+
+}
+  } // main end 
+
+
+    
+
+
+
+
+
